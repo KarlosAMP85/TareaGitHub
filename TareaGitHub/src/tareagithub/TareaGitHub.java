108a20be87ec4d589f4c919052dbe10edc279257
KarlosAMP85 <cpm0020@alu.medac.es>
2024-04-30 21:14:24 +0200

    version sobre la rama requisito

--- TareaGitHub/src/tareagithub/TareaGitHub.java
+++ TareaGitHub/src/tareagithub/TareaGitHub.java
@@ -14,7 +14,7 @@ public class TareaGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("pacomer");
+        System.out.println("pacomer2");
     }
     
 }
